3f27ef24e40656f045e5368bc8601b6e358297eb
alexnas <alexnas2python@gmail.com>
2020-05-12 19:22:55 +0300

    Make toolbar menu and sidebar menu with drawer.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <template>
   <v-app>
-    <v-navigation-drawer app temporary="false">
-      <!-- -->
+    <v-navigation-drawer app temporary="false" v-model="drawer">
+      <v-list>
+        <v-list-item v-for="link of links" :key="link.title" :to="link.url">
+          <v-list-item-icon>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title v-text="link.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar dark color="primary" app>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>App Service</v-toolbar-title>
-
+      <v-app-bar-nav-icon @click="drawer = !drawer" class="hidden-md-and-up"></v-app-bar-nav-icon>
+      <v-toolbar-title>Ad Service</v-toolbar-title>
       <v-spacer></v-spacer>
 
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn text>Link Two</v-btn>
-        <v-btn text>Link Two</v-btn>
-        <v-btn text>Link Two</v-btn>
+        <v-btn text v-for="link in links" :key="link.title" :to="link.url">
+          <v-icon left>{{ link.icon }}</v-icon>
+          {{ link.title }}
+        </v-btn>
       </v-toolbar-items>
     </v-app-bar>
 
@@ -25,7 +34,20 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { title: "Login", icon: "mdi-lock", url: "/login" },
+        { title: "Registration", icon: "mdi-face", url: "/registration" },
+        { title: "Orders", icon: "mdi-bookmark-outline", url: "/orders" },
+        { title: "New ad", icon: "mdi-file-plus", url: "/new" },
+        { title: "My ads", icon: "mdi-format-list-bulleted", url: "/list" }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,8 +6,7 @@ Vue.use(VueRouter);
 const routes = [];
 
 const router = new VueRouter({
-  // mode: 'history',
-  // base: process.env.BASE_URL,
+  mode: 'history',
   routes,
 });
 
