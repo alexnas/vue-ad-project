3fff9565fa907dbf00ba97a45543386c33c7ae21
alexnas <alexnas2python@gmail.com>
2020-05-13 23:30:48 +0300

    Fill the page (NewAd) with Ad Creation Form.

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,8 +1,56 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>New Ad</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text-secondary mb3">Create new ad</h1>
+        <v-form ref="form" v-model="valid" validation class="mb-3">
+          <v-text-field
+            label="Ad title"
+            name="title"
+            type="text"
+            v-model="title"
+            required
+            :rules="[v => !!v || 'Title is required']"
+          ></v-text-field>
+
+          <v-textarea
+            label="Ad description"
+            name="description"
+            type="text"
+            v-model="description"
+            :rules="[v => !!v || 'Description is required']"
+          ></v-textarea>
+        </v-form>
+        <v-layout row mb-3>
+          <v-flex xs12>
+            <v-btn class="warning">
+              Upload
+              <v-icon right>mdi-cloud-upload</v-icon>
+            </v-btn>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <img src="" height="100" />
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <v-switch
+              v-model="promo"
+              class="mx-2"
+              label="Add to promo"
+              color="primary"
+            ></v-switch>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <v-btn :disabled="!valid" class="success" @click="createAd">
+              Create ad
+            </v-btn>
+          </v-flex>
+        </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
@@ -11,10 +59,28 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      title: "",
+      description: "",
+      promo: false,
+      valid: false
+    };
+  },
+  methods: {
+    createAd() {
+      console.log("createAd");
+      if (this.$refs.form.validate()) {
+        // logic
+        const ad = {
+          title: this.title,
+          description: this.description,
+          promo: this.promo
+        };
+        console.log("ad", ad);
+      }
+    }
   }
 };
 </script>
 
-<style>
-</style>
+<style></style>
--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,27 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>Orders</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary" mb3>Orders</h1>
+        <v-list subheader two-line flat>
+          <v-list-item avatar v-for="order in orders" :key="order.id">
+            <v-list-item-action>
+              <v-checkbox
+                color="success"
+                v-model="order.done"
+                @click="markDone(order)"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ order.name }}</v-list-item-title>
+              <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn class="primary" :to="'/ad/' + order.adId">Open</v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
       </v-flex>
     </v-layout>
   </v-container>
@@ -11,10 +30,24 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      orders: [
+        {
+          id: "weoru32", // id for order
+          name: "Alex", // userName
+          phone: "8-921-212-2323",
+          adId: "123", // id for ad
+          done: false
+        }
+      ]
+    };
+  },
+  methods: {
+    markDone(order) {
+      order.done = true;
+    }
   }
 };
 </script>
 
-<style>
-</style>
+<style></style>
