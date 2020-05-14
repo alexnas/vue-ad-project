6872d66a172726ac4b0de9c20ddeb202a35bac26
alexnas <alexnas2python@gmail.com>
2020-05-14 13:45:58 +0300

    Create firebase account and initialize firebase in the project.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import "vuetify/dist/vuetify.min.css";
+import * as fb from "firebase";
 
 import App from "./App.vue";
 import router from "./router";
@@ -12,5 +13,16 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    fb.initializeApp({
+      apiKey: "AIzaSyAv_GtbVVx2kJUvjINh3U0uNLhsc_YQ2vY",
+      authDomain: "vue-ads-project-a1dff.firebaseapp.com",
+      databaseURL: "https://vue-ads-project-a1dff.firebaseio.com",
+      projectId: "vue-ads-project-a1dff",
+      storageBucket: "vue-ads-project-a1dff.appspot.com",
+      messagingSenderId: "598665661834",
+      appId: "1:598665661834:web:5c9cb62e0aa27758eccc73"
+    });
+  }
 }).$mount("#app");
